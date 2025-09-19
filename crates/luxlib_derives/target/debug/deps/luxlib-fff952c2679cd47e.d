7c3ab07eed750a607b292522b2299f60498c8702
luxgile <bisslit.12.2@gmail.com>
2025-09-19 14:54:23 +0200

    removed ignored target folder

--- crates/luxlib_derives/target/debug/deps/luxlib-fff952c2679cd47e.d
+++
@@ -1,9 +0,0 @@
-/mnt/6f7e372e-8cd1-4f27-980d-5342a70722c5/dev/luxlib/crates/luxlib_derives/target/debug/deps/luxlib-fff952c2679cd47e.d: src/lib.rs Cargo.toml
-
-/mnt/6f7e372e-8cd1-4f27-980d-5342a70722c5/dev/luxlib/crates/luxlib_derives/target/debug/deps/libluxlib-fff952c2679cd47e.rmeta: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
--- crates/luxlib_derives/target/debug/deps/luxlib_derives-e700263823ceb08d.d
+++
@@ -1,9 +0,0 @@
-/mnt/6f7e372e-8cd1-4f27-980d-5342a70722c5/dev/luxlib/crates/luxlib_derives/target/debug/deps/luxlib_derives-e700263823ceb08d.d: src/lib.rs Cargo.toml
-
-/mnt/6f7e372e-8cd1-4f27-980d-5342a70722c5/dev/luxlib/crates/luxlib_derives/target/debug/deps/libluxlib_derives-e700263823ceb08d.rmeta: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
